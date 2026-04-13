fd503fed604ce5a7ae17182926115d618dc43e3e
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-04-13 16:11:12 +0200

    CS fixes - native_function_invocation & static_lambda

--- Tests/LazyStringTest.php
+++ Tests/LazyStringTest.php
@@ -19,7 +19,7 @@ class LazyStringTest extends TestCase
     public function testLazyString()
     {
         $count = 0;
-        $s = LazyString::fromCallable(function () use (&$count) {
+        $s = LazyString::fromCallable(static function () use (&$count) {
             return ++$count;
         });
 
@@ -33,7 +33,7 @@ class LazyStringTest extends TestCase
      */
     public function testReturnTypeError()
     {
-        $s = LazyString::fromCallable(fn () => []);
+        $s = LazyString::fromCallable(static fn () => []);
 
         $this->expectException(\TypeError::class);
         $this->expectExceptionMessageMatches('{^Return value of .*\{closure.*\}\(\) passed to '.preg_quote(LazyString::class).'::fromCallable\(\) must be of the type string, array returned\.$}');
@@ -44,7 +44,7 @@ class LazyStringTest extends TestCase
     public function testLazyCallable()
     {
         $count = 0;
-        $s = LazyString::fromCallable([function () use (&$count) {
+        $s = LazyString::fromCallable([static function () use (&$count) {
             return new class($count) {
                 private int $count;
 
